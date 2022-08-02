b58d9f55fac86a948869128666ee84f0b47ced2f
huoyinghui <2285020853@qq.com>
2022-08-02 20:10:50 +0800

    update: test

--- testsDB/logic_dao_test.go
+++ testsDB/logic_dao_test.go
@@ -2,10 +2,11 @@ package testsDB
 
 import (
 	"admin/internal/dao"
+	"admin/internal/model/entity"
+	"context"
 	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/os/gctx"
-	"github.com/gogf/gf/v2/os/gtime"
 	"testing"
 )
 
@@ -19,16 +20,21 @@ func TestOrmUser(t *testing.T) {
 }
 func TestOrmUserInsert(t *testing.T) {
 	ctx := gctx.New()
-	data := g.Map{
-		"name":        "john",
-		"password":    "123",
-		"create_time": gtime.Now(),
-		"update_time": gtime.Now(),
-		"is_deleted":  0,
-	}
-	r, err := dao.Users.Ctx(ctx).Data(data).Insert()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(r)
+	//data := g.Map{
+	//	"name":        "john",
+	//	"password":    "123",
+	//	"create_time": gtime.Now(),
+	//	"update_time": gtime.Now(),
+	//	"is_deleted":  0,
+	//}
+	err := dao.Users.Transaction(ctx, func(ctx context.Context, tx *gdb.TX) error {
+		user := &entity.Users{}
+		user.Password = "123"
+		// 自动生成头像
+		r, err := dao.Users.Ctx(ctx).Data(user).OmitEmpty().Save()
+		t.Log(r)
+		return err
+	})
+	t.Log(err)
+	//r, err := dao.Users.Ctx(ctx).Data(data).Insert()
 }
