6160c0e17251d705b7f39d3baf549c8a0331a201
huoyinghui <2285020853@qq.com>
2022-08-03 17:07:57 +0800

    add: curd

--- internal/logic/users/users.go
+++ internal/logic/users/users.go
@@ -46,6 +46,38 @@ func (s *sUser) Login(ctx context.Context, in model.UserLoginInput) (uid int64, 
 	return uid, nil
 }
 
+// user reset api curd
+// get /api/user/ 返回用户列表
+// post /api/user/ 创建用户
+// get /api/user/{pk}/ 获取pk用户详情
+// put /api/user/pk/
+//  delete /api/user/pk/ 删除确定用户
+//  delete /api/user [] 删除确定用户
+
+// Register 执行注册
+func (s *sUser) Register(ctx context.Context, in model.UserLoginInput) (uid int64, err error) {
+	return
+}
+
+func (s *sUser) Create(ctx context.Context, in model.UserLoginInput) (uid int64, err error) {
+	return
+}
+
+// Update 执行注册
+func (s *sUser) Update(ctx context.Context, in model.UserLoginInput) (uid int64, err error) {
+	return
+}
+
+// Detail 执行注册
+func (s *sUser) Detail(ctx context.Context, in model.UserLoginInput) (uid int64, err error) {
+	return
+}
+
+// Delete 执行注册
+func (s *sUser) Delete(ctx context.Context, in model.UserLoginInput) (uid int64, err error) {
+	return
+}
+
 // Logout 注销
 func (s *sUser) Logout(ctx context.Context) error {
 	//return service.Session().RemoveUser(ctx)
