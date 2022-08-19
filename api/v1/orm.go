b220fe9804c082c5d4d22f00b096f46638386e01
huoyinghui <2285020853@qq.com>
2022-08-19 16:22:22 +0800

    orm

--- api/v1/orm.go
+++ api/v1/orm.go
@@ -3,13 +3,20 @@ package v1
 type OrmIdReq struct {
 	Id int `v:"required|min:1#id不能为空|id应当>=1" json:"id" dc:"id"`
 }
+
+type OrmSortTypeReq struct {
+	// 默认:0 create_at desc 1:crete_at asc 2:update_at desc 3:update asc
+	Type int `d:"0" json:"type"`
+}
+
+//----------上面req  下面res
+
 type OrmDeleteRes struct {
 }
 
 type OrmUpdateRes struct {
 }
 
-type OrmSortTypeReq struct {
-	// 默认:0 create_at desc 1:crete_at asc 2:update_at desc 3:update asc
-	Type int `d:"0" json:"type"`
+type OrmIdRes struct {
+	Id int `json:"id" dc:"id"`
 }
