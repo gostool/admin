c234ad0dd6ca02cd37569fdff99a37d997920878
tphyhFighting <2363176358@qq.com>
2022-08-21 09:59:10 +0800

    update: tree_test

--- library/tree/tree_test.go
+++ library/tree/tree_test.go
@@ -1,38 +1,35 @@
 package tree
 
 import (
-	"admin/internal/model/serializer"
-	"reflect"
 	"testing"
 )
 
 func TestBfs(t *testing.T) {
 
-	type args struct {
-		treeMap map[int][]*Node[serializer.RoleDetail]
-	}
-	child :=
-	treeMap := map[int][]*Node[serializer.RoleDetail]{
-		0: []*Node[serializer.RoleDetail]{},
-	}
-	tests := []struct {
-		name      string
-		args      args
-		wantItems []*Node[serializer.RoleDetail]
-	}{
-		// TODO: Add test cases.
-		{
-			name: "01",
-			args: args{
-				treeMap: treeMap,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotItems := Bfs(tt.args.treeMap); !reflect.DeepEqual(gotItems, tt.wantItems) {
-				t.Errorf("Bfs() = %v, want %v", gotItems, tt.wantItems)
-			}
-		})
-	}
+	//type args struct {
+	//	treeMap map[int][]*Node[serializer.RoleDetail]
+	//}
+	//child :=
+	//treeMap := map[int][]*Node[serializer.RoleDetail]{
+	//	0: []*Node[serializer.RoleDetail]{},
+	//}
+	//tests := []struct {
+	//	name      string
+	//	args      args
+	//	wantItems []*Node[serializer.RoleDetail]
+	//}{
+	//	{
+	//		name: "01",
+	//		args: args{
+	//			treeMap: treeMap,
+	//		},
+	//	},
+	//}
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//		if gotItems := Bfs(tt.args.treeMap); !reflect.DeepEqual(gotItems, tt.wantItems) {
+	//			t.Errorf("Bfs() = %v, want %v", gotItems, tt.wantItems)
+	//		}
+	//	})
+	//}
 }
