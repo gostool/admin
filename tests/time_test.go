aab18ad7f7888f0a49d04faef01a121b99c0769b
tphyhFighting <2363176358@qq.com>
2022-08-25 13:04:37 +0800

    update: time_test

--- tests/time_test.go
+++ tests/time_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -8,4 +9,14 @@ import (
 func TestTime(t *testing.T) {
 	now := time.Unix(time.Now().Unix(), 0)
 	t.Log(now)
+	const shortForm = "2006-Jan-02"
+	ti, _ := time.Parse(shortForm, "2012-Feb-03")
+	t.Log("time.Parse:", ti)
+
+	timer, err := time.Parse("2006-01-02 15:04:05", "2018-08-08 08:08:08")
+	if err != nil {
+		fmt.Printf("parse time failed, err:%v\n", err)
+		return
+	}
+	t.Logf("timeobj:%v\n", timer)
 }
