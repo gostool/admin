0c207c7a2e19ab74f406bb709bb6c6bbc3a00d6f
tphyhFighting <2363176358@qq.com>
2022-08-25 20:50:58 +0800

    update: casbin

--- internal/middleware/casbin.go
+++ internal/middleware/casbin.go
@@ -3,7 +3,10 @@ package middleware
 import (
 	"admin/internal/common"
 	"admin/internal/consts"
+	"errors"
+	"github.com/gogf/gf/v2/container/gmap"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 // Casbin
@@ -14,23 +17,23 @@ func Casbin(r *ghttp.Request) {
 	ctx := r.GetCtx()
 	roleId := common.GetVarFromCtx(ctx, consts.CtxUserRoleId).Int()
 	logger.Debugf(ctx, "roleId:%v method:%v url:%v is not admin\n", roleId, method, url)
-	//data := gmap.Map{}
-	//e, err := Casbin.GetEnforcer()
-	//if err != nil {
-	//	logger.Errorf(ctx, "GetEnforcer err:%v\n", err)
-	//	JsonExit(r, consts.ErrCodeErr, consts.ErrMsgErr, data)
-	//}
-	//success, err := e.Enforce(gconv.String(roleId), url, method)
-	//if err != nil {
-	//	logger.Errorf(ctx, "Enforce err:%v\n", err)
-	//	JsonExit(r, consts.ErrCodeErr, consts.ErrMsgErr, data)
-	//}
-	//if success {
-	//	r.Middleware.Next()
-	//	return
-	//} else {
-	//	err = errors.New("权限不足")
-	//	logger.Errorf(ctx, "roleId:%v method:%v url:%v err:%v\n", roleId, method, url, err)
-	//	JsonExit(r, consts.ErrCodeBad, err.Error(), data)
-	//}
+	data := gmap.Map{}
+	e, err := Casbin.GetEnforcer()
+	if err != nil {
+		logger.Errorf(ctx, "GetEnforcer err:%v\n", err)
+		JsonExit(r, consts.ErrCodeErr, consts.ErrMsgErr, data)
+	}
+	success, err := e.Enforce(gconv.String(roleId), url, method)
+	if err != nil {
+		logger.Errorf(ctx, "Enforce err:%v\n", err)
+		JsonExit(r, consts.ErrCodeErr, consts.ErrMsgErr, data)
+	}
+	if success {
+		r.Middleware.Next()
+		return
+	} else {
+		err = errors.New("权限不足")
+		logger.Errorf(ctx, "roleId:%v method:%v url:%v err:%v\n", roleId, method, url, err)
+		JsonExit(r, consts.ErrCodeBad, err.Error(), data)
+	}
 }
