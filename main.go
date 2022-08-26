96c5a9e0b457bea00aaba5c63e84daf3208e9923
huoyinghui <2285020853@qq.com>
2022-08-26 13:17:40 +0800

    update: main.go

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"admin/internal/cmd"
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/os/gctx"
+
 	_ "admin/internal/controller"
 	_ "admin/internal/logic"
 	_ "admin/internal/packed"
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-	"github.com/gogf/gf/v2/os/gcmd"
-
-	"github.com/gogf/gf/v2/os/gctx"
-
-	"admin/internal/cmd"
 )
 
 func main() {
